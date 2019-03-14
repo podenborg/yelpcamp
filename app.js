b41cf4ebf64c2162ff174ea455a862c220f9f4f7
Patrick O <patrick@odenborg.com>
2019-03-14 21:22:22 +0000

    add mongo atlas

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes      = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://dbadmin:password69@cluster0-l0np2.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
